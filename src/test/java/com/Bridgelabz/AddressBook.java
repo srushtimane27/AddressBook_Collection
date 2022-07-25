5e0c2579532f01a9b2fb8097c465a6ab40df8f76
srushtimane27 <srushtimane31@gmail.com>
2022-07-25 12:13:17 +0530

    Welcome to Address Book Program

---
+++ src/test/java/com/Bridgelabz/AddressBook.java
@@ -0,0 +1,23 @@
+package com.Bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    public static void main(String[] args) {
+
+        //Creating Address Book
+        List<String> list = new ArrayList<>();
+
+        list.add("Srushti");
+        list.add("Mane");
+        list.add("Dapodi");
+        list.add("Pune");
+        list.add("Maharashtra");
+        list.add("456874");
+        list.add("785423645");
+        list.add("abcd@gmail.com");
+
+        System.out.println(list);
+    }
+}
